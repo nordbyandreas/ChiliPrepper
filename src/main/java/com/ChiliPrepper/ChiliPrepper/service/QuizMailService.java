115f96bcc16950fcd998ca39c560dfa004ced6f7
christholest <christ.holest@outlook.com>
2017-03-17 10:33:47 +0100

    Finished QuizMailService interface

--- src/main/java/com/ChiliPrepper/ChiliPrepper/service/QuizMailService.java
+++ src/main/java/com/ChiliPrepper/ChiliPrepper/service/QuizMailService.java
@@ -1,7 +1,24 @@
 package com.ChiliPrepper.ChiliPrepper.service;
 
+import com.ChiliPrepper.ChiliPrepper.model.QuizMail;
+
+import java.util.List;
+
 /**
  * Created by Christer on 17.03.2017.
  */
 public interface QuizMailService {
+
+    Iterable<QuizMail> findAllByQuizmail_Id(Long id);
+    Iterable<QuizMail> findAllByQuiz_Id(Long id);
+    Iterable<QuizMail> findAllByParticipant_Id(Long id);
+    Iterable<QuizMail> findAllByQuiz_IdAndCourse_Id(Long quizId, Long courseId);
+
+    QuizMail findOneByQuiz_Id(Long id);
+    QuizMail findOneByQuiz_IdAndParticipant_Id(Long quizId, Long participantId);
+
+    void deleteAllByQuiz_Id(Long id);
+    void deleteAllByPartcipant_Id(Long id);
+
+    void save(QuizMail quizMail);
 }
